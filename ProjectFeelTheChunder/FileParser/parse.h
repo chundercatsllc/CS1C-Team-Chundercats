9be3a735c598d960e3e5e53a6fac38511010ac49
jcstill <31301328+jcstill@users.noreply.github.com>
2019-04-11 18:05:03 -0700

    FML YO

---
+++ ProjectFeelTheChunder/FileParser/parse.h
@@ -0,0 +1,66 @@
+#ifndef __PARSE_H__
+#define __PARSE_H__
+#include <fstream>
+#include <string>
+#include <iostream>
+#include "../vectors.h"
+using namespace std;
+
+class parse{
+	public:
+		// Default constructor
+		parse();
+		
+		// Non-default constructor
+		parse(string);
+		
+		// Sets (new) filename
+		void set_file(string);
+		
+		// Pretty obvious
+		void parseInput(int REPLACE_WITH_SHAPE_CLASS);
+		
+		// NOT PART OF PARSER CLASS REMOVE BEFORE DEVELOPMENT:
+		void print();
+	private:
+		// File IO Variables:
+		string ShapeFileName;
+		ifstream inputFile;
+		
+		// Set Defaults:
+		void setDefaults();
+		
+		// File Controller Stuffs:
+		void openFile();	
+		void closeFile();
+		
+		// String Parser:
+		void parseString();
+		
+		// Vector for dimensions string since it can theoretically be infinitely long:
+		Vectors<string> shpdmnsns;
+		
+		// "save" input to Shape object:
+		void storeInput(int REPLACE_WITH_SHAPE_CLASS);
+		
+		// Shape variables for inputing:
+		int ShapeId;
+		string Dimensions;
+		string ShapeType;
+		string PenColor;
+		int PenWidth;
+		string PenStyle;
+		string PenCapStyle;
+		string PenJoinStyle;
+		string BrushColor;
+		string BrushStyle;
+		string TextString;
+		string TextColor;
+		string TextAlignment;
+		int TextPointSize;
+		string TextFontFamily;
+		string TextFontStyle;
+		string TextFontWeight;
+};
+
+#endif
---
+++ README.md
@@ -0,0 +1,8 @@
+# CS1C-Team-Chundercats
+2D Modelling software
+
+Below is the link to the Agile user stories and various share docs
+https://drive.google.com/open?id=1TrWvyWAglxqE0wXDu3N-lB-7Bfl8fsZC
+
+slack link
+chundercatsunite.slack.com
