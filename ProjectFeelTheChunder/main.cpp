2cb33e636ed4f2035a64b8ff9be9e60dfff32b33
jcstill <jacobcstill@gmail.com>
2019-04-16 13:06:53 -0700

    Add test file parser to QT project. - Works

--- ProjectFeelTheChunder/main.cpp
+++ ProjectFeelTheChunder/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "./FileParser/parse.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -6,6 +7,13 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
+
+	// Temporary file parser test:
+	parse file1;
+	file1.set_file("shapes.txt");
+
+	file1.parseInput(1);
+	// ---------------------------
 
 	return a.exec();
 }
