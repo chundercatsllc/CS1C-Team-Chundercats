80ba1d864398c43552f03e5d0029e16ffa5eefa0
jcstill <jacobcstill@gmail.com>
2019-04-20 11:26:52 -0700

    Rectangle Class

--- ProjectFeelTheChunder/ShapeClass/rectangle.cpp
+++ ProjectFeelTheChunder/ShapeClass/rectangle.cpp
@@ -1,17 +1,18 @@
 #include "rectangle.h"
 
-Rectangle::Rectangle(){
-
+void Rectangle::move(const int x, const int y) override{
+	QPoint tmp(x, y);
+    coordinate = tmp;
 }
-void Rectangle::move(){
-	
+void Rectangle::draw(QPaintDevice *device) override{
+	painter.begin(device);
+	painter.setPen(penType);
+	painter.setBrush(brushType);
+	painter.drawRect(coordinate.x(),coordinate.y(), width,height);
+	painter.drawText(coordinate,QString::number(getId()));
+	painter.end();
 }
-void Rectangle::draw(){
-	
-}
-void Rectangle::perimeter(){
-	
-}
-void Rectangle::area(){
-	
+void Rectangle::setDimensions(int width, int height){
+	this->width = width;
+	this->height = height;
 }
