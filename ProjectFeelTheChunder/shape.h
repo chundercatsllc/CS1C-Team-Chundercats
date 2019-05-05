550aa3f8f318e5d29c6fb344a172e45aeae196ac
kpjr11814 <kpjr11814@gmail.com>
2019-05-04 20:06:15 -0700

    D-OX Commenting for Shape and Polygon

--- ProjectFeelTheChunder/shape.h
+++ ProjectFeelTheChunder/shape.h
@@ -11,7 +11,7 @@
 #include "containers/AwesomeVector.h"
 
 
-//!Shape Class inheriting QPainter Class.
+//!Shape Class: Public Inheritance from QPainter Class.
 
 /*!The Shape Class has public inheritance from the QPainter class. The QPainter Class contains various methods that allows any classes inheriting from the it have drawing attributes and other functionality. The Shape class is an abstract data type as the functions void move, void draw, double perimeter, and double area are all pure virtual functions.
 */
@@ -46,14 +46,29 @@ public:
     /*!Deallocates any memory stored in the heap. */
     virtual ~Shape(){}
 
+    //!setShape takes in a ShapeType argument
+    /*
+    The passed in argument must be of ShapeType. setShape sets shape to the ShapeType argument.
+    */
     void setShape(ShapeType);
+
+    //!setBrush takes two
     void setBrush(Qt::GlobalColor, Qt::BrushStyle);
     void setPen(Qt::GlobalColor);
     void setPen(Qt::GlobalColor, int width, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle);
 
+    //!setBrush takes in a QBrush variable and sets brush to brsh
+    /* \param brsh */
     void setBrush(QBrush brsh){brush = brsh;}
+
+    //!setPen takes in a QPen variable and sets pen to pn
+    /* \param pn */
     void setPen(QPen pn)      {pen   = pn;}
+
+    //!setID takes in a int ID and sets id to ID
+    /* \param ID */
     void setID(int ID)        {id    = ID;}
+
 
     ShapeType     getShape() const {return shape;}
     const QBrush &getBrush() const {return brush;}
@@ -62,37 +77,120 @@ public:
 
     void setDefaultStyle();
 
+    //!Pure Virtual move function
+    /*!
+    \param tX is of type const int
+    \param tY is of type const int
+    \param ptIndex is of type int
+    */
     virtual void move(const int tX, const int tY, int ptIndex) = 0;
+
+    //!Pure Virtual draw function
+    /*!
+    \param *device is of type QPaintDevice
+    */
     virtual void draw(QPaintDevice *device) = 0;
+
+    //!Pure Virtual perimeter function
+    /*!No default parameters */
     virtual double perimeter() = 0;
+
+    //!Pure Virtual area function
+    /*!No default parameters */
     virtual double area() = 0;
+
 protected:
+
+
     QPainter &getQPainter();
 
 private:
+
+    //!id is of type int
     int id;
+
+    //!shape is of type ShapeType
     ShapeType shape;
 };
 
+
 const int DEFAULT_NUM_VERTS = 8;
+
+//!Polygon Class: Public Inheritance from Shape Class
+
 class Polygon : public Shape
 {
 public:
+
+    //!Polygon Class constructor
+    /*!
+    \param *dev is a pointer of type QPaintDevice, intialized to nullptr to avoid conflicts
+    \param id is of type int
+    */
     Polygon(QPaintDevice* dev = nullptr, int id = -1);
+
+    //!Polygon Class Destructor
     ~Polygon() override {}
 
+    //!draw function to draw a Polygon
+    /*!
+    draw function overrides the virtual draw from abstract class Shape. Function does not return any type.
+    \param *dev is a pointer of type QPaintDevice.
+    */
     virtual void   draw(QPaintDevice *dev) override;
+
+    //!move function to move a polygon
+    /*!
+    move function overrides the virtual move from abstract base class Shape. Function does not return any type.
+    \param x is of type int
+    \param y is of type int
+    \param vertex is of type int
+    */
     virtual void   move(int x, int y, int vertex) override;
+
+    //!area function to calculate area of polygon
+    /*!
+    area function overrides the virtual area from abstract base class Shape. Function returns a double.
+    area function does not take in any parameters.
+    */
     virtual double area() override;
+
+    //!perimter function to calculate perimeter of polygon
+    /*!
+    perimeter function overrides the virtual perimeter abstract base class Shape. Function returns a double.
+    perimeter function does not take in any parameters.
+    */
     virtual double perimeter() override;
 
+    //!setNumVertices function sets the nummber of vertices for the polygon
+    /*!
+    numVerts is set equal to numVertices
+    \param numVertices is of type int
+    */
     void setNumVertices(int numVertices);
+
+    //!getNumVertices function returns the value of numVerts.
+    /*!No Parameters required, return type if of type int*/
     int  getNumVertices();
+
+    //!function to add a vertex to a polygon
+    /*!
+    addVertex function requires one parameter, no type is returned by the function
+    \param vertex is constant reference of type QPoint []
+    */
     void addVertex(const QPoint& vertex);
+
+
     AwesomeVector<QPoint>& getVertices();
+
 private:
+
     AwesomeVector<QPoint> vertVect;
+
+    //!Array of type QPoint, contains number of vertices for polygon
     QPoint vertsArray[DEFAULT_NUM_VERTS];
+
+    //!Variable to store the number of vertices in polygon
     int     numVerts;
 };
 
